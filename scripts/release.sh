aaf6f9f9484ac990530f28cde37e1bfb7ba7e7f1
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2023-11-09 19:51:25 -0800

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -96,7 +96,5 @@ git push --tags $REMOTE
 if [[ -e "${GOLLYX_HELLMOUTH_PELICAN_HOME}/environment.${PROMOTE_DEST_BRANCH}" ]]; then
     source "${GOLLYX_HELLMOUTH_PELICAN_HOME}/environment.${PROMOTE_DEST_BRANCH}"
     make -C "$GOLLYX_HELLMOUTH_PELICAN_HOME" deploy
-else
-    echo "Could not find environment config file ${GOLLYX_HELLMOUTH_PELICAN_HOME}/environment.${PROMOTE_DEST_BRANCH}. Unable to deploy."
 fi
 
